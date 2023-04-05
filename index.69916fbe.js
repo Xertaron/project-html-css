42ed7e4d79eb85b2549bae7eed305ca5aabe4075
Xertaron <122622349+Xertaron@users.noreply.github.com>
2023-04-05 17:24:47 +0000

    Deploying to gh-pages from @ Xertaron/project-html-css@a75f90ddadeb1677ef6f315d36e6d231291663b0 🚀

---
+++ index.69916fbe.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("backdrop__is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.69916fbe.js.map
