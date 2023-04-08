fe3050a3e53b1ec24aa937b6cb181b613945c1b7
Xertaron <xertaron@gmail.com>
2023-04-08 19:29:08 +0200

    zaczynamy podpinać modal

---
+++ src/js/modal-blog.js
@@ -0,0 +1,20 @@
+(() => {
+  const refs = {
+    openModalBtn1: document.querySelector('[data-modal-open1]'),
+    openModalBtn2: document.querySelector('[data-modal-open2]'),
+    openModalBtn3: document.querySelector('[data-modal-open3]'),
+    openModalBtn4: document.querySelector('[data-modal-open4]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal-blog]'),
+  };
+
+  refs.openModalBtn1.addEventListener('click', toggleModal);
+  refs.openModalBtn2.addEventListener('click', toggleModal);
+  refs.openModalBtn3.addEventListener('click', toggleModal);
+  refs.openModalBtn4.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
