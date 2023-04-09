43f7325ba63830edc38708d37d95cfe894c777ad
Xertaron <xertaron@gmail.com>
2023-04-09 12:44:08 +0200

    zadziała?

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,11 +2,7 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
-  const closeMenuItem1 = document.querySelector('.js-close-item-1');
-  const closeMenuItem2 = document.querySelector('.js-close-item-2');
-  const closeMenuItem3 = document.querySelector('.js-close-item-3');
-  const closeMenuItem4 = document.querySelector('.js-close-item-4');
-  const closeMenuItem5 = document.querySelector('.js-close-item-5');
+  const closeMenuItem = document.querySelector('.js-close-item');
 
   const toggleMenu = () => {
     const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
@@ -19,11 +15,7 @@
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuItem1.addEventListener('click', toggleMenu);
-  closeMenuItem2.addEventListener('click', toggleMenu);
-  closeMenuItem3.addEventListener('click', toggleMenu);
-  closeMenuItem4.addEventListener('click', toggleMenu);
-  closeMenuItem5.addEventListener('click', toggleMenu);
+  closeMenuItem.addEventListener('click', toggleMenu);
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
