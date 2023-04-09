d57e4570d9d3220b1f71b31bd1a9baf1fa534cd1
Xertaron <xertaron@gmail.com>
2023-04-09 23:31:24 +0200

    jestem zmęczony

--- src/js/modal.js
+++ src/js/modal.js
@@ -5,6 +5,7 @@
     openModalBtn3: document.querySelector('[data-modal-open3]'),
     openModalBtn4: document.querySelector('[data-modal-open4]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
+    continueModalBtn: document.querySelector('[data-modal-continue]'),
     modal: document.querySelector('[data-modal]'),
   };
 
@@ -13,6 +14,7 @@
   refs.openModalBtn3.addEventListener('click', toggleModal);
   refs.openModalBtn4.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.continueModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
