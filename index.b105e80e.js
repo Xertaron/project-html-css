aa467fe6899bfb758dd39a667b232f98c0919a04
Xertaron <122622349+Xertaron@users.noreply.github.com>
2023-04-09 17:12:38 +0000

    Deploying to gh-pages from @ Xertaron/project-html-css@e74bd8ce120f10312d2010584e66a1bf654efc0b 🚀

---
+++ index.b105e80e.js
@@ -0,0 +1,2 @@
+(()=>{const o={openModalBlogBtn:document.querySelector("[data-modal-blog-open]"),closeModalBlogBtn:document.querySelector("[data-modal-blog-close]"),modal:document.querySelector("[data-modal-blog]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBlogBtn.addEventListener("click",e),o.closeModalBlogBtn.addEventListener("click",e)})();
+//# sourceMappingURL=index.b105e80e.js.map
