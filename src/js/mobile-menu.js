f41b9b3f97427b1aaeb323384abc6835f25aed32
Xertaron <xertaron@gmail.com>
2023-04-09 12:53:34 +0200

    go

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,7 +2,6 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
-  const closeMenuItem = document.querySelector('.js-close-item');
 
   const toggleMenu = () => {
     const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
@@ -15,7 +14,6 @@
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuItem.addEventListener('click', toggleMenu);
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
