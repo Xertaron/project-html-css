ed2ccf860866a0145d506205e32ef55d99ad9a8b
Xertaron <xertaron@gmail.com>
2023-04-09 22:38:31 +0200

    modal

--- src/js/modal-thank-you.js
+++ src/js/modal-thank-you.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
-    openModalThankYouBtn: document.querySelector('[data-modal-thanks-open]'),
-    closeModalThankYouBtn: document.querySelector('[data-modal-thanks-close]'),
-    continueModalThankYou: document.querySelector('[data-modal-thanks-continue]'),
-    modalThankYou: document.querySelector('[data-modal-thanks]'),
+    openModalThanksBtn: document.querySelector('[data-modal-thanks-open]'),
+    closeModalThanksBtn: document.querySelector('[data-modal-thanks-close]'),
+    continueModalThanks: document.querySelector('[data-modal-thanks-continue]'),
+    modal: document.querySelector('[data-modal-thanks]'),
   };
 
-  refs.openModalThankYouBtn.addEventListener('click', toggleModal);
-  refs.closeModalThankYouBtn.addEventListener('click', toggleModal);
-  refs.continueModalThankYou.addEventListener('click', toggleModal);
+  refs.openModalThanksBtn.addEventListener('click', toggleModal);
+  refs.closeModalThanksBtn.addEventListener('click', toggleModal);
+  refs.continueModalThanks.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
